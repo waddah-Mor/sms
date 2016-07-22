7a475b33ba650247257a18a950a6f2ba3a6896eb
Liam Jones <liam@plumbnation.co.uk>
2016-07-22 15:58:51 +0100

    fs structure mapped in gateway test, nothing implemented at all though

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -1,10 +1,31 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use org\bovigo\vfs\vfsStream;
+use Sms\Gateway;
 use Sms\MessageReceived;
 
 class GatewayTest extends TestCase
 {
+	private $root;
+
+	public function setUp()
+	{
+		$structure = [
+			"checked"  => [],
+			"failed"   => [],
+			"incoming" => [],
+			"outgoing" => [],
+			"sent"     => []
+		];
+
+		$this->root = vfsStream::setup(
+			"/var/spool/sms",
+			null,
+			$structure
+		);
+	}
+
 	public function testSpoolDirectoryDoesNotExist()
 	{
 	}
